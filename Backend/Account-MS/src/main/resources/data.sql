502f3c84ea12a70774bf5878ff2a1e6f515f42c4
soniyasam <soniyamarysam@gmail.com>
2021-08-18 17:48:27 +0530

    All Microservices added

--- Backend/Account-MS/src/main/resources/data.sql
+++ Backend/Account-MS/src/main/resources/data.sql
@@ -1,5 +1,11 @@
 INSERT INTO ACCOUNT(account_Id,customer_Id,current_Balance,account_Type,opening_date,owner_Name)
-VALUES (1000000001,'CUSTOMER101',80000.00, 'Savings',TO_DATE('09/10/2021', 'DD/MM/YYYY'), 'Harish');
+VALUES (1000000001,'CUSTOMER101',80000.00, 'Savings',TO_DATE('09/02/2021', 'DD/MM/YYYY'), 'Soniya Sam');
 
 INSERT INTO ACCOUNT(account_Id,customer_Id,current_Balance,account_Type,opening_date,owner_Name)
-VALUES (1000000002, 'CUSTOMER102', 2000.00, 'Current', TO_DATE('09/10/2021', 'DD/MM/YYYY'),'Vimal');
+VALUES (1000000002, 'CUSTOMER102', 2000.00, 'Current', TO_DATE('21/10/2021', 'DD/MM/YYYY'),'Akash A');
+
+INSERT INTO ACCOUNT(account_Id,customer_Id,current_Balance,account_Type,opening_date,owner_Name)
+VALUES (1000000003, 'CUSTOMER103', 20000.00, 'Savings', TO_DATE('18/05/2021', 'DD/MM/YYYY'),'Trimu');
+
+INSERT INTO ACCOUNT(account_Id,customer_Id,current_Balance,account_Type,opening_date,owner_Name)
+VALUES (1000000004, 'CUSTOMER104', 10000.00, 'Current', TO_DATE('01/07/2021', 'DD/MM/YYYY'),'Trimu');
--- Backend/Authentication-MS/target/classes/data.sql
+++ Backend/Authentication-MS/target/classes/data.sql
@@ -1,6 +1,7 @@
 insert into appuser (userid,username,password,role) values ('EMPLOYEE101','employee','employee','EMPLOYEE');
-insert into appuser (userid,username,password,role) values ('CUSTOMER101','customer','customer','CUSTOMER');
-insert into appuser (userid,username,password,role) values ('CUSTOMER102','customer','customer','CUSTOMER');
-insert into appuser (userid,username,password,role) values ('EMPLOYEE102','employee','employee','EMPLOYEE');
-insert into appuser (userid,username,password,role) values ('EMPLOYEE103','employee','employee','EMPLOYEE');
-insert into appuser (userid,username,password,role) values ('EMPLOYEE104','soniya','soniya','EMPLOYEE');
+insert into appuser (userid,username,password,role) values ('SONIYAEMP102','soniya','soniya','EMPLOYEE');
+insert into appuser (userid,username,password,role) values ('CUSTOMER101','Soniya Sam','customer','CUSTOMER');
+insert into appuser (userid,username,password,role) values ('CUSTOMER102','Akash A','customer','CUSTOMER');
+insert into appuser (userid,username,password,role) values ('CUSTOMER103','Trimu','employee','CUSTOMER');
+insert into appuser (userid,username,password,role) values ('CUSTOMER104','Rajeshwar','employee','CUSTOMER');
+
